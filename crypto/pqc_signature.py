0b0585b83ac24e97fb8532fc8c6c4b04b1b33c03
C Sairamsuhaas Reddy <22091a05d9@rgmcet.edu.in>
2026-02-05 16:12:42 +0530

    Update pqc_signature.py

--- crypto/pqc_signature.py
+++ crypto/pqc_signature.py
@@ -1,1 +1,22 @@
+import oqs
+import hashlib
+
+SIG_ALG = "ML-DSA-44"
+
+def sign_metadata(data: bytes):
+    """
+    Signs ledger metadata using ML-DSA (Dilithium).
+    """
+    sig = oqs.Signature(SIG_ALG)
+    public_key = sig.generate_keypair()
+    signature = sig.sign(data)
+    return public_key, signature
+
+
+def verify_metadata(data: bytes, signature: bytes, public_key: bytes):
+    """
+    Verifies ML-DSA signature.
+    """
+    sig = oqs.Signature(SIG_ALG)
+    return sig.verify(data, signature, public_key)
 
