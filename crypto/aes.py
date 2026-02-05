901fd78f58d79ed71c6cc4b843fbbe547287fc04
C Sairamsuhaas Reddy <22091a05d9@rgmcet.edu.in>
2026-02-05 16:08:21 +0530

    Implement AES encryption and decryption functions

--- crypto/aes.py
+++ crypto/aes.py
@@ -1,1 +1,13 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
+
+def aes_encrypt(data, key):
+    iv = os.urandom(12)
+    enc = Cipher(algorithms.AES(key), modes.GCM(iv)).encryptor()
+    ct = enc.update(data) + enc.finalize()
+    return iv, ct, enc.tag
+
+def aes_decrypt(iv, ct, tag, key):
+    dec = Cipher(algorithms.AES(key), modes.GCM(iv, tag)).decryptor()
+    return dec.update(ct) + dec.finalize()
 
