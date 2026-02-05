b95672fe7b04e113cee817cd24103ee86b2ee6c5
C Sairamsuhaas Reddy <22091a05d9@rgmcet.edu.in>
2026-02-05 16:09:02 +0530

    Update pqc_kem.py

--- crypto/pqc_kem.py
+++ crypto/pqc_kem.py
@@ -1,1 +1,12 @@
+import oqs
+
+def kem_encapsulate():
+    kem = oqs.KeyEncapsulation("Kyber512")
+    pk = kem.generate_keypair()
+    ct, ss = kem.encap_secret(pk)
+    return pk, ct, ss
+
+def kem_decapsulate(ct):
+    kem = oqs.KeyEncapsulation("Kyber512")
+    return kem.decap_secret(ct)
 
