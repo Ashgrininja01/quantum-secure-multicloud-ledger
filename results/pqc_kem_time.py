61803defc97252c95418876948d1b09bd3e5df5c
C Sairamsuhaas Reddy <22091a05d9@rgmcet.edu.in>
2026-02-07 18:53:00 +0530

    Update pqc_kem_time.py

--- results/pqc_kem_time.py
+++ results/pqc_kem_time.py
@@ -6,17 +6,19 @@ dec_times = []
 
 for _ in range(5):
     t0 = time.time()
-    ct, _ = kem_encapsulate()
+    ct, sk = kem_encapsulate()
     enc_times.append(time.time() - t0)
 
     t0 = time.time()
-    kem_decapsulate(ct)
+    kem_decapsulate(ct, sk)
     dec_times.append(time.time() - t0)
 
-plt.bar(["Encapsulation", "Decapsulation"],
-        [sum(enc_times)/len(enc_times), sum(dec_times)/len(dec_times)])
+plt.bar(
+    ["Encapsulation", "Decapsulation"],
+    [sum(enc_times)/len(enc_times), sum(dec_times)/len(dec_times)]
+)
 plt.ylabel("Time (seconds)")
 plt.title("PQC KEM Overhead (Kyber)")
 plt.grid(True)
-plt.savefig("results/graphs/pqc_kem.png")
+plt.savefig("results/plots/pqc_kem.png")
 plt.show()
