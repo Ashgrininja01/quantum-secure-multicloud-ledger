9029b5dadc598a425c121ce3f5f7257fff82e9c5
C Sairamsuhaas Reddy <22091a05d9@rgmcet.edu.in>
2026-02-07 18:47:25 +0530

    Create pqc_kem_time.py

---
+++ results/pqc_kem_time.py
@@ -0,0 +1,22 @@
+import time, matplotlib.pyplot as plt
+from crypto.pqc_kem import kem_encapsulate, kem_decapsulate
+
+enc_times = []
+dec_times = []
+
+for _ in range(5):
+    t0 = time.time()
+    ct, _ = kem_encapsulate()
+    enc_times.append(time.time() - t0)
+
+    t0 = time.time()
+    kem_decapsulate(ct)
+    dec_times.append(time.time() - t0)
+
+plt.bar(["Encapsulation", "Decapsulation"],
+        [sum(enc_times)/len(enc_times), sum(dec_times)/len(dec_times)])
+plt.ylabel("Time (seconds)")
+plt.title("PQC KEM Overhead (Kyber)")
+plt.grid(True)
+plt.savefig("results/graphs/pqc_kem.png")
+plt.show()
