6323826571c50f5f913ef447e282d61a9243f951
C Sairamsuhaas Reddy <22091a05d9@rgmcet.edu.in>
2026-02-07 18:51:57 +0530

    Update aes_performance.py

--- results/aes_performance.py
+++ results/aes_performance.py
@@ -1,6 +1,5 @@
 import os, time, matplotlib.pyplot as plt
 from crypto.aes import aes_encrypt, aes_decrypt
-import os
 
 sizes_kb = [1, 10, 50, 100, 500]
 enc_times = []
@@ -8,22 +7,21 @@ dec_times = []
 
 for size in sizes_kb:
     data = os.urandom(size * 1024)
-    key = os.urandom(32)
 
     t0 = time.time()
-    iv, ct = aes_encrypt(data, key)
+    iv, ct = aes_encrypt(data)
     enc_times.append(time.time() - t0)
 
     t0 = time.time()
-    aes_decrypt(iv, ct, key)
+    aes_decrypt(iv, ct)
     dec_times.append(time.time() - t0)
 
-plt.plot(sizes_kb, enc_times, label="Encryption")
-plt.plot(sizes_kb, dec_times, label="Decryption")
+plt.plot(sizes_kb, enc_times, marker="o", label="Encryption")
+plt.plot(sizes_kb, dec_times, marker="o", label="Decryption")
 plt.xlabel("File Size (KB)")
 plt.ylabel("Time (seconds)")
 plt.title("AES-256 Performance vs File Size")
 plt.legend()
 plt.grid(True)
-plt.savefig("results/graphs/aes_performance.png")
+plt.savefig("results/plots/aes_performance.png")
 plt.show()
