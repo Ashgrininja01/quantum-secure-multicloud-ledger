37465d6f7f64488a7ae4b958ec3534b9f83da4a0
C Sairamsuhaas Reddy <22091a05d9@rgmcet.edu.in>
2026-02-07 17:59:34 +0530

    Update storage_client.py

--- cloud/storage_client.py
+++ cloud/storage_client.py
@@ -1,1 +1,16 @@
 
+# cloud/storage_client.py
+
+from cloud.aws_client import upload as aws_upload
+from cloud.azure_client import upload as azure_upload
+from cloud.gcp_client import upload as gcp_upload
+
+def replicate(file_path: str):
+    """
+    Replicates encrypted files across multiple clouds
+    """
+    aws_upload(file_path)
+    azure_upload(file_path)
+    gcp_upload(file_path)
+
+    print("[Multi-Cloud] Replication completed")
